20de8b339d291885cb84c19b9baa1db565638534
Nguyễn Trung Kiên <138596085+nakien0205@users.noreply.github.com>
2025-02-19 08:10:30 +0700

    Update w_main.py

--- BERT/Whisper/w_main.py
+++ BERT/Whisper/w_main.py
@@ -1,7 +1,24 @@
 import torch
 from TTS.api import TTS
+import gradio
 import pyaudio
 import wave
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+''' 
+model 1: tts_models/multilingual/multi-dataset/xtts_v2
+   - Is multi-lingual
+   - Synthesis speech more percisely but slower
+'''
+
+'''
+model 2: tts_models/en/ljspeech/fast_pitch
+    - Has en only
+    - Faster speech conversion
+'''
+# path for config : C:\Users\ADMIN\AppData\Local\tts
+# if use multi-lingual then add speaker and language
 
 class AudioFile:
     chunk = 1024
@@ -29,29 +46,23 @@ class AudioFile:
         self.stream.close()
         self.p.terminate()
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-''' 
-model 1: tts_models/multilingual/multi-dataset/xtts_v2
-   - Is multi-lingual
-   - Synthesis speech more percisely but slower
-'''
-
-'''
-model 2: tts_models/en/ljspeech/fast_pitch
-    - Has en only
-    - Faster speech conversion
-'''
-# path for config : C:\Users\ADMIN\AppData\Local\tts
-
 def generate_audio(text):
     tts = TTS("tts_models/en/ljspeech/fast_pitch").to(device)
-    tts.tts_to_file(text, speaker="Ana Florence", language='en', file_path='output.wav')
-    return 'output.wav'
+    tts.tts_to_file(text, file_path='outputs/output.wav')
+    return 'outputs/output.wav'
 
-audio = generate_audio('Justin Beaver is a popular singer!')
+
+audio = generate_audio('What are you doing here?')
+
+demo = gradio.Interface(
+    fn=generate_audio,
+    inputs=[gradio.Text(label="Text"),],
+    outputs=[gradio.Audio(label="Audio"),],
+)
+demo.launch()
 
 # Usage example for pyaudio
-a = AudioFile(audio)
-a.play()
-a.close()
+
+# a = AudioFile(audio)
+# a.play()
+# a.close()
